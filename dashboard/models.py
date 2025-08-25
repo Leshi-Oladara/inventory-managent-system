f035c952c8e0859523dd5c86df63354799dafd57
LESHI OLADARA <leshisimeon@gmail.com>
2025-08-25 21:09:16 +0100

    removed template

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+CATEGORY=(
+    ("drugs", "drugs"),
+    ("fruits", "fruits"),
+    ("food","food"),
+)
+
+class product(models.Model):
+    name = models.CharField(max_length=100, blank=True)
+    category= models.CharField(max_length=100, choices=CATEGORY,blank=True)
+    quantity= models.PositiveIntegerField(null=True)
